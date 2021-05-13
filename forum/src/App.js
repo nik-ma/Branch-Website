81e9cd410a59617ccfabd86fada766f5377b3e20
nik-ma <nikhilmaurya5618056@gmail.com>
2021-05-13 12:15:40 +0530

    added navbar

--- forum/src/App.js
+++ forum/src/App.js
@@ -1,11 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <>
+      
+      <Router>
+        <Navbar />
+        
+      </Router>
+    
+    </>
   );
 }
 
