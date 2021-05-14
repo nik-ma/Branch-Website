27b90d5d0848f8293a901bf63f148b6053623f0d
nik-ma <nikhilmaurya5618056@gmail.com>
2021-05-14 10:40:47 +0530

    testing

--- forum/src/components/Herosection.js
+++ forum/src/components/Herosection.js
@@ -4,10 +4,18 @@ function Herosection() {
     return (
         <>
         <div className="heroContainer">
-        <video src='video-2.mp4' autoPlay loop muted />
-        </div>
+        {/* <video src='video-2.mp4' autoPlay loop muted /> */}
+        
         <h1 className="glow">Samnavay 21</h1>
-        {/* <h1 className="tagline">We are together</h1> */}
+        <h1 className="tagline">Your limitation-it's only your imagination</h1>
+        <div className="Cards">
+            <div className="CardItem">
+                <img src="electronics.jpg" alt="electronics" style={{width:'40px'}} />
+                Copmetitions
+            </div>
+        </div>
+        
+        </div>
         </>
     )
 }
