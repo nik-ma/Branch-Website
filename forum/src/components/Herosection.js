02b5d712ef7200f3670600a26d51680e21f12e47
nik-ma <nikhilmaurya5618056@gmail.com>
2021-05-14 13:20:26 +0530

    added cards

--- forum/src/components/Herosection.js
+++ forum/src/components/Herosection.js
@@ -4,18 +4,33 @@ function Herosection() {
     return (
         <>
         <div className="heroContainer">
-        {/* <video src='video-2.mp4' autoPlay loop muted /> */}
-        
+            
+        {/* <video src='video-2.mp4'  autoPlay loop muted /> */}
         <h1 className="glow">Samnavay 21</h1>
         <h1 className="tagline">Your limitation-it's only your imagination</h1>
+        
+        
         <div className="Cards">
             <div className="CardItem">
-                <img src="electronics.jpg" alt="electronics" style={{width:'40px'}} />
-                Copmetitions
+                <img className="imging" src="electronics.jpg" alt="electronics"  />
+                    
+            </div>
+            <div className="CardItem">
+                <img className="imging" src="robot.jpg" alt="electronics"  />
+                    
+            </div>
+            
+            <div className="CardItem">
+                <img className="imging" src="contact.jpg" alt="electronics"  />
+                    
+            </div>
+            <div className="CardItem">
+                <img className="imging" src="alumni.jpg" alt="electronics"  />
+                    
             </div>
         </div>
-        
         </div>
+        
         </>
     )
 }
