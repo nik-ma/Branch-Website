1a4335222ae11c230cb9d736fd6bf8e50e1fa88a
nik-ma <nikhilmaurya5618056@gmail.com>
2021-05-26 13:05:44 +0530

    structure done

--- forum/src/components/pages/mystery.js
+++ forum/src/components/pages/mystery.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import './mystery.css'
+function values(){
+    let content="hello i am your content";
+};
 function mystery() {
     return (
+        <>
         <div className="textstyle">
+        <div className="sidebar">
+            
+            <button>
+                Event1
+            </button>
+            <button>
+                Event2
+            </button>
+        </div>
             
             <div className="eventname">
                     <h1 className="heading"> 
@@ -10,13 +23,23 @@ function mystery() {
                     _______________Murder Mystery_______________</h1>
                 </div>
             <div className="content">
-                hello
+            <h1 className="heading">
+                i am your content
+                 </h1>
+               
             </div>
-            <div className="contact">
+            <div className="eventname">
+                <h1 className="heading">
+                __________________Contact US_________________
+                </h1>
+                
 
             </div>
            
         </div>
+        
+        
+</>
     )
 }
 
